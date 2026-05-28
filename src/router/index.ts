d8d4fdd8789665b4724b10c15e6fc9475012024d
71858677-beep <71858677+71858677-beep@users.noreply.github.com>
2026-05-29 01:21:37 +0800

    fix: revert GH Pages base path for Vercel deployment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/Love-Diary/'),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
